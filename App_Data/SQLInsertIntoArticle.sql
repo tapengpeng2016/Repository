2d69c25fb39ced13cfd11110147766dea1de0aab
Shadi MATTAR <shadi@mattar.fr>
2016-10-20 15:47:33 +0200

    SQL For Reservation

--- App_Data/SQLInsertIntoArticle.sql
+++ App_Data/SQLInsertIntoArticle.sql
@@ -1,11 +1,16 @@
-﻿
-
+﻿CREATE PROCEDURE [ReserverArticle]
+ @idArticle int,
+ @idUsager int
+AS
 INSERT INTO [dbo].[Reservation] 
 ([Id_Usager], [Id_Exemplaire], [Date_Reservation])
 VALUES 
-(@idArticle, 
+(@Usager, 
 (SELECT TOP 1 X.Id_Exemplaire
 	FROM [dbo].[Exemplaire] as X
 	WHERE X.Id_Article = @idArticle
-	AND X.Id_Exemplaire NOT IN (SELECT Id_Exemplaire FROM [dbo].[Reservation])), 
+	AND X.Id_Exemplaire NOT IN (SELECT Id_Exemplaire 
+									FROM [dbo].[Reservation] 
+									WHERE [Id_Usager] = @idUsager)), 
 GETDATE());
+GO
